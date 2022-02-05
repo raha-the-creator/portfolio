5315740aad160cd437da6deed2e2d9b0949ae0cf
raha-the-creator <duisek.kyzylorda@gmail.com>
2022-02-04 17:55:25 -0800

    turtle

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Raha <a href="https://nextjs.org">Cherepaha</a>
+          Raha <a href="https://nextjs.org">Cherepaha🐢</a>
         </h1>
 
         <p className={styles.description}>
