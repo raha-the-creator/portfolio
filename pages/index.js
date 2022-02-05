d7a587fd91c1b1f69c1e0c83f9ca56c2ae623c34
raha-the-creator <duisek.kyzylorda@gmail.com>
2022-02-04 17:53:41 -0800

    meme

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Раха <a href="https://nextjs.org">Черепаха</a>
         </h1>
 
         <p className={styles.description}>
