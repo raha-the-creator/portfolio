93d05810d97aa9e8ac2af0be3bcf58e0bdf1c3ca
raha-the-creator <duisek.kyzylorda@gmail.com>
2022-02-10 10:41:13 -0800

    asdf

--- comp/Project/index.js
+++ comp/Project/index.js
@@ -104,7 +104,7 @@ const ProjectDescM = styled.div`
 const { MediaContextProvider, Media } = createMedia({
     // breakpoints values can be either strings or integers
     breakpoints: {
-      sm: 600,
+      sm: 1080,
       md: 768,
       lg: 1024,
       xl: 1192,
