63543ba61a2fdc7343bbc782850a0d55f92fa532
raha-the-creator <duisek.kyzylorda@gmail.com>
2022-06-07 23:30:25 -0700

    asdf

--- pages/index.js
+++ pages/index.js
@@ -108,8 +108,8 @@ export default function Home() {
         <div className={styles.section1} data-aos="fade-down">
           <div className={styles.box1}>
             <Header>Hello, <span className={styles.name}>I am Raha!🐢</span></Header>
-            <span className={styles.name}>Front-end Developer/UI Designer</span>
-            <Par>Front-End Developer adept in all stages of advanced web development. Knowledgeable in user interface, testing, and debugging processes. Proficient in HTML, CSS3/SCSS, JavaScript | DOM, as well as other Front-End tools, libraries, and frameworks. Able to effectively self-manage during independent projects, as well as collaborate in a team setting.</Par>
+            <span className={styles.name}>Front-end Developer/UI Designer.</span>
+            <Par>Front-End Developer adept in all stages of advanced web development. Knowledgeable in user interface, testing, and debugging processes. Proficient in HTML, CSS3/SCSS, JavaScript | ES6 | DOM, as well as other Front-End tools, libraries, and frameworks. Able to effectively self-manage during independent projects, as well as collaborate in a team setting.</Par>
             <Par>In my leasure time, I enjoy playing guitar, exercising, reading, learning history and languages.</Par>
             <ResumeButton />
           </div>
