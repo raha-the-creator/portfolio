cb429cf229b3560f0ae16a6fc193cdd9f4727a9b
raha-the-creator <duisek.kyzylorda@gmail.com>
2022-08-21 14:42:40 -0700

    updated resume

--- comp/Resume/index.js
+++ comp/Resume/index.js
@@ -23,7 +23,7 @@ const ResumeButton = ({}) =>{
 
     return (
     <ButtonWrapper>
-        <a href="https://drive.google.com/file/d/1w5xovbwfdwv511VaUSTxtFTeoaBYDQC4/view?usp=sharing">
+        <a href="https://drive.google.com/file/d/1TUek2_-ITQqzMTV885p45FgcFrUR84w7/view?usp=sharing">
             <Text>See My Resume</Text>
         </a>
     </ButtonWrapper>
