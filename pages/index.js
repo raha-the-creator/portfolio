f631eb3ab5f2f812c36661b82ef23a65c0537a3b
raha-the-creator <duisek.kyzylorda@gmail.com>
2022-10-24 13:09:53 -0700

    asdf

--- pages/index.js
+++ pages/index.js
@@ -231,55 +231,6 @@ export default function Home() {
               prlink="http://thriveclub.ca/"
               prpage="https://www.figma.com/proto/pYEz4vK8vSywHSkhWrronF/portfolio-website?page-id=253%3A68&node-id=270%3A121&viewport=241%2C48%2C0.42&scaling=min-zoom"
             />
-            <Line />
-            <ProjectCard
-              img="./farmtotable.png"
-              title="Farm To Table"
-              year="2021"
-              type="Front-end Development and UI/UX"
-              desc="First project using Next.js. I contributed a lot in designing the mockups and building them using styled-components and Next.js"
-              prlink="https://farmtotable.vercel.app/"
-              prpage="https://www.figma.com/proto/pYEz4vK8vSywHSkhWrronF/portfolio-website?page-id=253%3A68&node-id=253%3A88&viewport=241%2C48%2C0.38&scaling=min-zoom"
-            />
-            <Line />
-            {/* <ProjectCard
-              img="./oncampus.png"
-              title="onCampus food delivery"
-              year="2021"
-              type="UI/UX"
-              desc="Won 3rd place in BCIT Innovation Challenge and 3rd place at Nourishing Innovation Challenge. Designed additional service in BCIT mobile application to help students who reside in dormitory to receive their groceries on time and accordingly. Source: https://www.linkedin.com/feed/update/urn:li:activity:6820875880559185920/"
-              prlink="https://www.figma.com/proto/tmangvaPSepSjvuozsMXdm/onCampus-delivery?page-id=5%3A116&node-id=21%3A1&starting-point-node-id=21%3A1"
-            />
-            <Line />
-            <ProjectCard
-              img="./wck.png"
-              title="West Coast Kids Foundation"
-              year="2021"
-              type="Front-end Development"
-              desc="Worked with WCK team to maintain WCK's website, including but not limited to its functionality and performance.
-              Assisted the team in updating the website as needed.
-              Made suggestions to the WCK team to improve the website and implement them as needed. Improvements included, but weren't limited to, adding helpful features, optimizing the website's performance, improving its responsiveness, and improving the user experience through conducting usability testings"
-              prlink="https://www.wckfoundation.ca/"
-            />
-            <Line />
-            <ProjectCard
-              img="./westcoast.png"
-              title="West Point Hotel"
-              year="2021"
-              type="Front-end Development"
-              desc="Term project to develop website using CMS (WordPress). Developed custom theme and added additional features (360 view of the room, etc.)"
-              prlink="http://hoppeyyy.com/west_point_hotel/"
-            />
-            <Line />
-            <ProjectCard
-              img="./canadacomputers.png"
-              title="Canada Computers"
-              year="2020"
-              type="UI/UX"
-              desc="Rapid prototyping and complete redesign of Canada Computers. I have contributed into a creating low and high fidelity testings which later on been taken into creating final version of the website."
-              prlink="https://www.figma.com/proto/OvgIZAjwznDuaAyAy7p8mh/Canada-Computers-Redesign?page-id=0%3A1&node-id=1%3A2&starting-point-node-id=1%3A2"
-            />
-            <Line /> */}
           </div>
         </div>
 
